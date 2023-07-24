f3afef21de551a41be4ad7c78cf9b4cb171ba8b8
Roberto.Felipe <roberto.oliveira@anchieta.br>
2023-07-24 18:01:15 -0300

    feat: create Login page

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className=" flex h-[50%] w-[50%] rounded-lg  border-[1px] border-lightblue-100 bg-black p-12">
+        <h1 className="w-full text-center text-2xl font-light text-white">
+          Acesse sua conta
+        </h1>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  darkMode: ["class", '[data-mode="dark"]'],
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        gray: {
+          50: '#F0F0F0',
+          100: '#E1DFE2',
+          200: '#939393',
+          300: '#6C6C6C',
+          400: '#2B2B2B'
+        },
+        lightblue: {
+          100: '#00E0FF'
+        },
+        yellow: { 800: '#FFB22B' },
+        success: '#67A950',
+        error: '#BE3737'
+      },
+      screens: {
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280px'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
