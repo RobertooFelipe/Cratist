a1c13b58f97b8b32be050c6f9a6bbfb71a2cfb43
RobertooFelipe <feliperoberto092@gmail.com>
2023-08-01 14:42:05 -0300

    feat: :sparkles: add new properties in Login page

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,11 +1,34 @@
-export default function Login() {
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import { GiSwordsEmblem } from 'react-icons/gi'
+
+export default async function Login() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <div className=" flex h-[50%] w-[50%] rounded-lg  border-[1px] border-lightblue-100 bg-black p-12">
-        <h1 className="w-full text-center text-2xl font-light text-white">
-          Acesse sua conta
-        </h1>
+    <>
+      <div className="absolute bottom-2 flex w-full items-center justify-center gap-2 opacity-20">
+        <h1 className="text-xl font-semibold text-white">Cratist</h1>
+        <GiSwordsEmblem color="#fff" size={30} />
+        <p className="text-sm text-white">&copy;Powered by RobertooFelipe</p>
       </div>
-    </div>
+
+      <div className="flex h-screen w-screen items-center justify-center">
+        <div className="flex h-[350px] w-[700px] flex-col justify-between rounded-lg border-[1px] border-lightblue-100 bg-black p-8">
+          <div>
+            <h1 className="font-regular w-full text-center text-2xl text-white">
+              Access your account
+            </h1>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Input type="text" placeholder="Username" />
+            <Input type="password" placeholder="Password" />
+          </div>
+
+          <div>
+            <Button>Acessar</Button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
